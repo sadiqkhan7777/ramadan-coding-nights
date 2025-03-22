6d8271e6fc6179ab37d9a9446fced5bfaf57472d
sadiqkhan7777 <saiqkhan7777@gmail.com>
2025-03-22 02:06:05 -0700

    Added Day-15-simple-agent

---
+++ Day-15-simple-agent/main.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+provider = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai",
+)
+
+model = OpenAIChatCompletionsModel(model="gemini-2.0-flash", openai_client=provider)
+
+agent = Agent(
+    name="Greeting Agent",
+    instructions="You are a Greeting Agent, Your task is to greet the user with a friendly message, when someone says hi you've reply back with salam from Sadiq khan, if someone says bye then say allah hafiz from Sadiq khan, when someone asks other than greeting then say Sadiq is here just for greeting, I can't answer anything else, sorry.",
+    model=model,
+)
+
+user_question = input("Please enter your question: ")
+
+result = Runner.run_sync(agent, user_question)
+
+print(result.final_output)
